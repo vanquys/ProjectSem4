532acdae869e0f241d61d33f3bdfc0a52c123502
Van Quy <quyvan2003@gmail.com>
2023-08-19 17:12:32 +0700

    commit

--- src/main/java/com/aptech/ProjectSem4/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aptech.ProjectSem4.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<Object, Long>{
-	
-	
-}
--- src/test/java/com/aptech/ProjectSem4/ProjectSem4ApplicationTests.java
+++ src/test/java/com/aptech/ProjectSem4/ProjectSem4ApplicationTests.java
@@ -3,7 +3,7 @@ package com.aptech.ProjectSem4;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = ProjectSem4Application.class)
+@SpringBootTest
 class ProjectSem4ApplicationTests {
 
 	@Test
