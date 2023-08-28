ada61a2ebc2c94c3e029571fe0ca85057122cdb2
phanquan1201 <89560503+phanquan1201@users.noreply.github.com>
2023-08-28 18:29:52 +0700

    Add files via upload

--- src/main/java/com/aptech/ProjectSem4/controllers/AdCourseController.java
+++ src/main/java/com/aptech/ProjectSem4/controllers/AdCourseController.java
@@ -9,90 +9,73 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.aptech.ProjectSem4.entities.Courses;
 import com.aptech.ProjectSem4.reponsitory.CourseRepository;
+import com.aptech.ProjectSem4.service.CoursesService;
+
+
 
 @Controller
-@RequestMapping("/admin/courses")
+@RequestMapping("/admin")
 public class AdCourseController {
-	
-	
-	private CourseRepository _courseRepository;
-	
 	@Autowired
-	public AdCourseController(CourseRepository courseRepository) {
-		this._courseRepository = courseRepository;
+	private CoursesService _coursesService;
+	
+	@GetMapping("/ViewAdCourses")
+	public String ViewAdCourses(Model model) {
+	model.addAttribute("ListCourses" , _coursesService.getAllCourses());
+	return "adminCourses";
 	}
 	
-	@GetMapping
-	public ResponseEntity<List<Courses>> getAllCourses(){
-		List<Courses> courses = (List<Courses>) _courseRepository.findAll();
-		return ResponseEntity.ok(courses);
+	@GetMapping("/getModelEdit")
+	public ResponseEntity<Courses> getModelEdit(int id) {
+	Courses course =  _coursesService.getCourseById(id);
+	return ResponseEntity.ok(course);
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Courses> getCourseById(@PathVariable Integer id){
-		Optional<Courses> optionalCourse = (Optional<Courses>) _courseRepository.findById(id);
-		return optionalCourse.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-	
-	}
-	
-	@PostMapping
-	public ResponseEntity<Courses> createCourse(@RequestBody Courses course){
-		Courses createdCourse = _courseRepository.save(course);
-		List<Courses> updatedCourses = (List<Courses>) _courseRepository.findAll();
-		return ResponseEntity.status(HttpStatus.CREATED).body(createdCourse);
-		
-	}
-	
-	@PutMapping("/{id}") 
-    public ResponseEntity<Courses> updateCourse(@PathVariable Integer id, @RequestBody  Courses updatedCourses) {
-        if (!_courseRepository.existsById(id)) {
-            return ResponseEntity.notFound().build(); 
-        }
-        updatedCourses.setId(id); 
-        Courses savedCourse = _courseRepository.save(updatedCourses); 
-        return ResponseEntity.ok(savedCourse); 
-	}
-	@DeleteMapping("/{id}") 
-    public ResponseEntity<Void> deleteCourse(@PathVariable Integer id) {
-        if (!_courseRepository.existsById(id)) {
-            return ResponseEntity.notFound().build();
-        }
-        
-        _courseRepository.deleteById(id); 
-        return ResponseEntity.noContent().build(); 
+    @ResponseBody
+    public Courses getCourseById(@PathVariable int id) {
+        return _coursesService.getCourseById(id);
     }
-	 @GetMapping("/search")
-	    public ResponseEntity<List<Courses>> searchCourses(@RequestParam(required = false) Integer id, @RequestParam(required = false) String name) {
-	        List<Courses> courses;
 
-	        if (id != null) {
-	            // Tìm kiếm theo ID
-	            Optional<Courses> optionalCourse = (Optional<Courses>) _courseRepository.findById(id);
-	            if (optionalCourse.isPresent()) {
-	                courses = Collections.singletonList(optionalCourse.get());
-	            } else {
-	                return ResponseEntity.notFound().build();
-	            }
-	        } else if (name != null) {
-	            // Tìm kiếm theo tên
-	            courses = _courseRepository.findByNameContainingIgnoreCase(name);
-	        } else {
-	            return ResponseEntity.badRequest().build();
-	        }
+    @PostMapping("/edit")
+    @ResponseBody
+    public String processEditForm(@ModelAttribute Courses updatedCourse) {
+    	_coursesService.updateCourse(updatedCourse.getId(), updatedCourse);
+        return "Success";
+    }
 
-	        return ResponseEntity.ok(courses);
-	    }
+    @PostMapping("/add")
+    @ResponseBody
+    public String processAddForm(@ModelAttribute Courses newCourse) {
+    	_coursesService.saveCourse(newCourse);
+        return "Success";
+    }
+    @PostMapping("/save")
+    @ResponseBody
+    public String saveCourse(@ModelAttribute Courses course) {
+        _coursesService.saveCourse(course);
+        return "Success";
+    }
+    @PostMapping("/delete/{id}")
+    @ResponseBody
+    public String deleteCourse(@PathVariable int id) {
+        _coursesService.deleteCourse(id);
+        return "Success";
+    }
 	
-}
 	
+}	
