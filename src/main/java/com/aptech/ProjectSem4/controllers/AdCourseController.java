269600dbf18b309600ea4b7c18fd78aba4fd1afa
Van Quy <quyvan2003@gmail.com>
2023-09-17 15:00:47 +0700

    commit 3

--- src/main/java/com/aptech/ProjectSem4/controllers/AdCourseController.java
+++ src/main/java/com/aptech/ProjectSem4/controllers/AdCourseController.java
@@ -1,70 +1,106 @@
 package com.aptech.ProjectSem4.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.aptech.ProjectSem4.entities.Courses;
 import com.aptech.ProjectSem4.reponsitory.CourseRepository;
 
-import jakarta.security.auth.message.callback.PrivateKeyCallback.Request;
-
-
-
 @Controller
+
+<<<<<<< HEAD
+@RequestMapping("/admin/course")
+=======
 @RequestMapping("/admin")
+>>>>>>> df8ef064bc45eba9d94f8aadc20833a9220af01b
 public class AdCourseController {
 	@Autowired
 	private CourseRepository courseRepository;
-	
-	
+
 	@GetMapping("/ViewAdCourses")
 	public String ViewAdCourses(Model model) {
-	model.addAttribute("ListCourses" , courseRepository.findAll());
-	return "admin/ad_Course";
+		model.addAttribute("ListCourses", courseRepository.findAll());
+<<<<<<< HEAD
+		return "course";
+	}
+
+
+	@GetMapping("/editModel")
+	@ResponseBody
+	public ResponseEntity<Courses> getCourseById(@RequestParam int id) {
+		Courses course = courseRepository.findById(id).orElse(null);
+		if (course != null) {
+			return ResponseEntity.ok(course);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@GetMapping("/edit")
+	@ResponseBody
+	public String processEditForm(Courses updatedCourse) {
+		courseRepository.save(updatedCourse);
+		return "Success";
+	}
+
+//	// thêm khóa học
+	@GetMapping("/add")
+	@ResponseBody
+	public String saveAddForm(Courses newCourse) {
+		courseRepository.save(newCourse);
+		return "Success";
 	}
 	
 	
-	@GetMapping("/editModel")
-    public String getCourseById( Model model, int id) {
-		Courses courses = courseRepository.findById(id).get();
-		model.addAttribute("course", courses);
-        return "admin/course_detail";
-    }
+	
 
-	
-	
-	
-	// chỉnh sửa khóa học
-    @PostMapping("/edit")
-    @ResponseBody
-    public String processEditForm( Courses updatedCourse) {
-    	courseRepository.save( updatedCourse);
-        return "Success";
-    }
-    
-    // thêm khóa học
-    @PostMapping("/add")
-    @ResponseBody
-    public String processAddForm( Courses newCourse) {
-    	courseRepository.save(newCourse);
-        return "Success";
-    }
-    
-    // xóa khóa học
-    @PostMapping("/delete/{id}")
-    @ResponseBody
-    public String deleteCourse( int id) {
-        courseRepository.deleteById(id);
-        return "Success";
-    }
-	
-	
-}	
+	// xóa khóa học
+=======
+		return "ad_Courses";
+	}
+
+	@GetMapping("/editModel")
+	@ResponseBody
+	public ResponseEntity<Courses> getCourseById(@RequestParam int id) {
+		Courses course = courseRepository.findById(id).orElse(null);
+		if (course != null) {
+			return ResponseEntity.ok(course);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@GetMapping("/edit")
+	@ResponseBody
+	public String processEditForm(Courses updatedCourse) {
+		courseRepository.save(updatedCourse);
+		return "Success";
+	}
+
+	@GetMapping("/add")
+	@ResponseBody
+	public String saveAddForm(Courses newCourse) {
+		courseRepository.save(newCourse);
+		return "Success";
+	}
+
+>>>>>>> df8ef064bc45eba9d94f8aadc20833a9220af01b
+	@PostMapping("/delete/{id}")
+	@ResponseBody
+	public String deleteCourse(int id) {
+		courseRepository.deleteById(id);
+		return "Success";
+	}
+
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> df8ef064bc45eba9d94f8aadc20833a9220af01b
---
+++ src/main/java/com/aptech/ProjectSem4/controllers/AdTeacherController.java
@@ -0,0 +1,122 @@
+<<<<<<< HEAD
+package com.aptech.ProjectSem4.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.aptech.ProjectSem4.entities.Teachers;
+import com.aptech.ProjectSem4.reponsitory.TeacherRepository;
+
+@Controller
+@RequestMapping("/admin/teachers")
+public class AdTeacherController {
+
+	@Autowired
+	private TeacherRepository teacherRepository;
+
+	@GetMapping("/ViewAdTeachers")
+	public String ViewAdStudents(Model model) {
+		model.addAttribute("ListTeachers", teacherRepository.findAll());
+		return "teachers";
+	}
+
+	@GetMapping("/editModel")
+	@ResponseBody
+	public ResponseEntity<Teachers> getCourseById(@RequestParam int id) {
+		Teachers teacher = teacherRepository.findById(id).orElse(null);
+		if (teacher != null) {
+			return ResponseEntity.ok(teacher);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@GetMapping("/edit")
+	@ResponseBody
+	public String processEditForm(Teachers updatedTeacher) {
+		teacherRepository.save(updatedTeacher);
+		return "Success";
+	}
+
+	@GetMapping("/add")
+	@ResponseBody
+	public String saveAddForm(Teachers newTeacher) {
+		teacherRepository.save(newTeacher);
+		return "Success";
+	}
+
+	@PostMapping("/delete/{id}")
+	@ResponseBody
+	public String deleteTeacher(int id) {
+		teacherRepository.deleteById(id);
+		return "Success";
+	}
+
+}
+=======
+package com.aptech.ProjectSem4.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.aptech.ProjectSem4.entities.Teachers;
+import com.aptech.ProjectSem4.reponsitory.TeacherRepository;
+
+@RequestMapping("/admin")
+public class AdTeacherController {
+
+	@Autowired
+	private TeacherRepository teacherRepository;
+
+	@GetMapping("/ViewAdStudents")
+	public String ViewAdStudents(Model model) {
+		model.addAttribute("ListCourses", teacherRepository.findAll());
+		return "teacher";
+	}
+
+	@GetMapping("/editModel")
+	@ResponseBody
+	public ResponseEntity<Teachers> getCourseById(@RequestParam int id) {
+		Teachers teacher = teacherRepository.findById(id).orElse(null);
+		if (teacher != null) {
+			return ResponseEntity.ok(teacher);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	@GetMapping("/edit")
+	@ResponseBody
+	public String processEditForm(Teachers updatedTeacher) {
+		teacherRepository.save(updatedTeacher);
+		return "Success";
+	}
+
+	@GetMapping("/add")
+	@ResponseBody
+	public String saveAddForm(Teachers newTeacher) {
+		teacherRepository.save(newTeacher);
+		return "Success";
+	}
+
+	@PostMapping("/delete/{id}")
+	@ResponseBody
+	public String deleteTeacher(int id) {
+		teacherRepository.deleteById(id);
+		return "Success";
+	}
+
+}
+>>>>>>> df8ef064bc45eba9d94f8aadc20833a9220af01b
