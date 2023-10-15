c852b10bf8d94e8abac89973cc12a8b6ef90dfc7
Van Quy <quyvan2003@gmail.com>
2023-10-16 00:39:25 +0700

    up

--- src/main/java/com/aptech/ProjectSem4/reponsitory/NotificationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aptech.ProjectSem4.reponsitory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.aptech.ProjectSem4.entities.Notifications;
-
-public interface NotificationRepository extends JpaRepository<Notifications, Integer> {
-}
